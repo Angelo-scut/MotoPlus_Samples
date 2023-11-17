dc0c6f9055bdb9073d6a839b9f405ed1a790ddba
YanWenqian <yanwenqian_edu@163.com>
2023-11-17 09:29:59 +0800

    测试tcp变换矩阵的ui

--- wzs_control/test_ui/test_ui/main.cpp
+++ wzs_control/test_ui/test_ui/main.cpp
@@ -1,10 +1,12 @@
 #include "test_ui.h"
+//#include "tcp_test.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     test_ui w;
+    //tcp_test w;
     w.show();
     return a.exec();
 }
